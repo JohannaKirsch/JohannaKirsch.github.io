3498b54a4c2939a5a9ae925271331c5af676f4c8
Johanna <plueschigergehtsnicht@gmail.com>
2024-01-23 13:18:30 +0100

    scene 2 added

---
+++ js/scenes.js
@@ -0,0 +1,85 @@
+//overall variables
+let scenes = document.querySelectorAll('a-marker');
+let textElement = document.getElementById("text");
+
+//scene1 variables
+let scene1Over = false;
+let scene1Detected = false;
+let scene1 = scenes[0];
+var a = 0;
+var text1 = ["Das Ziel war Amerika. ▸"
+    , "102 Passagiere hofften auf einen Neuanfang im neuen Land. ▸"
+    , "Auf dem Schiff waren Familien, Männer, Frauen aber auch elternlose Kinder. ▸"
+    , "Diese Gruppe legte die Grundlage für die heutige USA."];
+
+//scene2 variables
+let scene2Over = false;
+let scene2Detected = false;
+let scene2 = scenes[1];
+var b = 0;
+var text2 = ["Alle Passagiere tranken Bier auf dem Schiff. ▸"
+    , "Das Wasser war nicht sauber genug. ▸"
+    , "Es gab auch Wein zum Trinken. ▸"
+    , "Viele tranken Zitronensaft, um die Krankheit Skorbut nicht zu bekommen. ▸"
+    , "Im Getränk Cold Duck auf der Karte ist beides zu finden: Wein und Zitronensaft."];
+
+/**
+ * Overall function for Clickevent
+ */
+function myFunction() {
+    scene1.addEventListener("markerFound", (e) => {
+        console.log("found");
+    });
+    startScene1();
+    startScene2();
+};
+
+//scene1.addEventListener("markerFound", (e) => { document.getElementById("turnPage").style.visibility = 'hidden' });
+
+/**
+ * scene1 function
+ */
+function startScene1() {
+    if (scene1Detected && !scene1Over) {
+        console.log("heyho");
+        if (text1[a] == null) {
+            scene1.removeChild(scene1.firstElementChild);
+            document.getElementById("turnPage").style.visibility = 'visible';
+            scene1Over = true;
+        }
+        if (a == 1) {
+            scene1.removeChild(scene1.firstElementChild);
+            scene1.removeChild(scene1.firstElementChild);
+            scene1.removeChild(scene1.firstElementChild);
+            let group = document.createElement('a-entity');
+            group.setAttribute('position', '0 0 0');
+            group.setAttribute('rotation', '0 0 0');
+            group.setAttribute('scale', '1 1 1');
+            group.setAttribute('animation-mixer', 'clip: *; loop:repeat');
+            group.setAttribute('gltf-model', '#group');
+            scene1.appendChild(group);
+        }
+        document.getElementById("text").textContent = text1[a];
+        a += 1;
+    };
+};
+
+/**
+ * scene2 function
+ */
+function startScene2() {
+    if (!scene2Detected) {
+        scene2.addEventListener("markerFound", (e) => {
+            document.getElementById("turnPage").style.visibility = 'hidden';
+            scene2Detected = true;
+        });
+    };
+    if (scene2Detected && !scene2Over) {
+        if (text2[b] == null) {
+            document.getElementById("turnPage").style.visibility = 'visible';
+            scene2Over = true;
+        }
+        document.getElementById("text").textContent = text2[b];
+        b += 1;
+    };
+};
